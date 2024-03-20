e9a9c88e9380ec11acdebc8ede2d0ab0b78977af
nir-g0 <ngub24@gmail.com>
2024-03-20 00:21:50 -0700

    Beginning of log in + dependencies
    working on UI for login screen, set default admin account for testing, added font dependencies, not yet implemented

---
+++ Atlas/First_page.tsx
@@ -0,0 +1,114 @@
+import React, { useState } from 'react';
+import {Text, View, StyleSheet, Dimensions, TextInput, TouchableOpacity, Image} from 'react-native';
+
+
+
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
+const FirstPageApp = () => {
+  const [email, setEmail] = useState('');
+  const[password, setPassword] = useState('');
+  const checkInput = () => {
+    if(email == 'ADMIN' && password == 'ADMINPASS'){
+        console.log("Logged in!")
+    }
+    else{
+      console.log("Log in!")
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>ATLAS</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        placeholderTextColor="#999"
+        keyboardType="email-address"
+        onChangeText={newText => setEmail(newText)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        placeholderTextColor="#999"
+        secureTextEntry
+        onChangeText={newText => setPassword(newText)}
+      />
+      <TouchableOpacity>      
+        <Text style={styles.forgotPassword}>Forget password?</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={checkInput}
+                        style={styles.loginButton}>
+        <Text style={styles.loginButtonText}>LOGIN</Text>
+      </TouchableOpacity>
+      <View style={styles.socialLoginContainer}>
+        <TouchableOpacity style={styles.socialLoginButton}>
+          <Image source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/2048px-Google_%22G%22_logo.svg.png'}}
+                 style={{width: screenHeight/20, height: screenHeight/20}} />
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity>
+        <Text style={styles.createAccount}>Create an Account</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const colorTheme = '#2596bd'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: screenHeight/18,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: colorTheme,
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: colorTheme,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  forgotPassword: {
+    marginBottom: 20,
+    color: colorTheme,
+  },
+  loginButton: {
+    width: '80%',
+    height: screenHeight/18,
+    backgroundColor: colorTheme,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    marginBottom: screenHeight/24,
+  },
+  loginButtonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  socialLoginContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+  socialLoginButton: {
+    width: screenHeight/20,
+    height: screenHeight/20,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 5,
+  },
+  createAccount: {
+    color: colorTheme,
+  },
+});
+
+export default FirstPageApp;
