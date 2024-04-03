1f300fb4cb8e5b4b5bfda3ea98e714ca84c3a130
TimothyPhan2 <98495706+TimothyPhan2@users.noreply.github.com>
2024-04-03 13:18:50 -0700

    Added IOS Location Permissions

--- Atlas/Screens/Map_Screen.tsx
+++ Atlas/Screens/Map_Screen.tsx
@@ -1,4 +1,12 @@
-import {View, Text, Button, PermissionsAndroid} from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  PermissionsAndroid,
+  Linking,
+  Alert,
+  Platform,
+} from 'react-native';
 import React, {useState} from 'react';
 import GeoLocation from 'react-native-geolocation-service';
 const Map_Screen = () => {
@@ -8,7 +16,7 @@ const Map_Screen = () => {
   });
 
   // Function to get permission for location mainly for android
-  const requestLocationPermission = async () => {
+  const requestLocationPermissionAndroid = async () => {
     try {
       const granted = await PermissionsAndroid.request(
         PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
@@ -32,6 +40,44 @@ const Map_Screen = () => {
     }
   };
 
+  const requestLocationPermissionIOS = async () => {
+    const openSetting = () => {
+      Linking.openSettings().catch(() => {
+        Alert.alert('Unable to open settings');
+      });
+    };
+    const status = await GeoLocation.requestAuthorization('whenInUse');
+
+    if (status === 'granted') {
+      console.log('Location permission granted');
+      return true;
+    }
+
+    if (status === 'denied') {
+      Alert.alert('Location permission denied');
+    }
+
+    if (status === 'disabled') {
+      Alert.alert(
+        `Turn on Location Services to allow "Atlas" to determine your location.`,
+        '',
+        [
+          {text: 'Go to Settings', onPress: openSetting},
+          {text: "Don't Use Location", onPress: () => {}},
+        ],
+      );
+    }
+
+    return false;
+  };
+
+  const requestLocationPermission = async () => {
+    if (Platform.OS === 'android') {
+      return await requestLocationPermissionAndroid();
+    } else {
+      return await requestLocationPermissionIOS();
+    }
+  };
   const getLocation = async () => {
     const res = await requestLocationPermission();
 
@@ -54,9 +100,10 @@ const Map_Screen = () => {
     <View>
       <Button title="Create Pin" onPress={getLocation} />
       <Text>Latitude: {currentLocation ? currentLocation.latitude : null}</Text>
-      <Text>Longitude: {currentLocation ? currentLocation.longitude : null}</Text>
+      <Text>
+        Longitude: {currentLocation ? currentLocation.longitude : null}
+      </Text>
     </View>
-
   );
 };
 
