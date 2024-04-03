0d8375323e5763cb66fdb8d766ca371250d72c2a
TimothyPhan2 <98495706+TimothyPhan2@users.noreply.github.com>
2024-04-02 22:48:43 -0700

    Implemented Geolocation API to get current location

--- Atlas/Screens/Map_Screen.tsx
+++ Atlas/Screens/Map_Screen.tsx
@@ -1,120 +1,63 @@
-import {SafeAreaView, StyleSheet, View} from 'react-native';
-import React from 'react'
-import MapView from 'react-native-maps';
+import {View, Text, Button, PermissionsAndroid} from 'react-native';
+import React, {useState} from 'react';
+import GeoLocation from 'react-native-geolocation-service';
 const Map_Screen = () => {
-  return (
-    <SafeAreaView style={{flex: 1}}>
-    <View style={styles.container}>
-      <MapView
-        style={styles.mapStyle}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-        customMapStyle={mapStyle}>
-      </MapView>
-    </View>
-  </SafeAreaView>
-  )
-}
-const mapStyle = [
-    {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-    {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-    {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-    {
-      featureType: 'administrative.locality',
-      elementType: 'labels.text.fill',
-      stylers: [{color: '#d59563'}],
-    },
-    {
-      featureType: 'poi',
-      elementType: 'labels.text.fill',
-      stylers: [{color: '#d59563'}],
-    },
-    {
-      featureType: 'poi.park',
-      elementType: 'geometry',
-      stylers: [{color: '#263c3f'}],
-    },
-    {
-      featureType: 'poi.park',
-      elementType: 'labels.text.fill',
-      stylers: [{color: '#6b9a76'}],
-    },
-    {
-      featureType: 'road',
-      elementType: 'geometry',
-      stylers: [{color: '#38414e'}],
-    },
-    {
-      featureType: 'road',
-      elementType: 'geometry.stroke',
-      stylers: [{color: '#212a37'}],
-    },
-    {
-      featureType: 'road',
-      elementType: 'labels.text.fill',
-      stylers: [{color: '#9ca5b3'}],
-    },
-    {
-      featureType: 'road.highway',
-      elementType: 'geometry',
-      stylers: [{color: '#746855'}],
-    },
-    {
-      featureType: 'road.highway',
-      elementType: 'geometry.stroke',
-      stylers: [{color: '#1f2835'}],
-    },
-    {
-      featureType: 'road.highway',
-      elementType: 'labels.text.fill',
-      stylers: [{color: '#f3d19c'}],
-    },
-    {
-      featureType: 'transit',
-      elementType: 'geometry',
-      stylers: [{color: '#2f3948'}],
-    },
-    {
-      featureType: 'transit.station',
-      elementType: 'labels.text.fill',
-      stylers: [{color: '#d59563'}],
-    },
-    {
-      featureType: 'water',
-      elementType: 'geometry',
-      stylers: [{color: '#17263c'}],
-    },
-    {
-      featureType: 'water',
-      elementType: 'labels.text.fill',
-      stylers: [{color: '#515c6d'}],
-    },
-    {
-      featureType: 'water',
-      elementType: 'labels.text.stroke',
-      stylers: [{color: '#17263c'}],
-    },
-  ];
-  const styles = StyleSheet.create({
-    container: {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      alignItems: 'center',
-      justifyContent: 'flex-end',
-    },
-    mapStyle: {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-    },
+  const [currentLocation, setCurrentLocation] = useState({
+    latitude: 0,
+    longitude: 0,
   });
-export default Map_Screen
+
+  // Function to get permission for location mainly for android
+  const requestLocationPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Geolocation Permission',
+          message: 'Atlas needs access to your location',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('Location permission granted');
+        return true;
+      } else {
+        console.log('Location permission denied');
+        return false;
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  const getLocation = async () => {
+    const res = await requestLocationPermission();
+
+    if (res) {
+      GeoLocation.getCurrentPosition(
+        position => {
+          const {latitude, longitude} = position.coords;
+          setCurrentLocation({latitude, longitude});
+        },
+        error => {
+          console.log(error.code, error.message);
+        },
+        {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+      );
+    }
+    console.log(currentLocation);
+  };
+
+  return (
+    <View>
+      <Button title="Create Pin" onPress={getLocation} />
+      <Text>Latitude: {currentLocation ? currentLocation.latitude : null}</Text>
+      <Text>Longitude: {currentLocation ? currentLocation.longitude : null}</Text>
+    </View>
+
+  );
+};
+
+export default Map_Screen;
